129aba115be2679b86e8c162d8f61b265c6f9760
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-03-18 13:50:53 +0100

    header

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require('require/header.php');
+
+
+?>
+
+<main>
+    <section id="accueil">
+        <h1>Bonjour et bienvenu sur mon Portefolio</h1>
+    </section>
+    <section class="cadre" id="humain"></section>
+    <section id="machine"></section>
+    <section id="porjet"></section>
+    <section id="contact"></section>
+    <section id="footer"></section>
+
+
+</main>
+
+
+<?php
+
+require('require/footer.php');
+?>
---
+++ require/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portefolio</title>
+    <link rel="stylesheet" href='css/header.css'>
+    <link rel="stylesheet" href='css/main.css'>
+    <link rel="stylesheet" href='css/footer.css'>
+    <!-- font Awesome -->
+    <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'>
+
+
+
+
+</head>
+<body>
+<header>
+    <nav>
+        <section>
+            <h2>Joris Verguldezoone</h2>
+            <img src="images/netero.png">
+            <p>Développeur Full-Stack</p>
+        </section>
+        <ul>
+            <li class="li_nav"><a class="a_nav"><i class="far fa-id-badge"></i>L'humain</a></li>
+            <li class="li_nav"><a class="a_nav"><i class="fas fa-robot"></i>La machine</a></li>
+            <li class="li_nav"><a class="a_nav"><i class="fas fa-tasks"></i>Mes projets</a></li>
+            <li class="li_nav"><a class="a_nav"><i class="far fa-envelope"></i>Me contacter</a></li>
+        </ul>
+    </nav>
+</header>
+    
